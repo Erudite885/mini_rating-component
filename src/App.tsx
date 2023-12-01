0d203dd40a6abec845c0356aeb04d9d1b4508db8
Erudite885 <erudite885@gmail.com>
2023-12-02 02:46:00 +1300

    removed thankyou component from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
 import RatingComponent from "./components/RatingComponent"
-import Thankyou from './components/Thankyou'
 
 const App: React.FC = () => {
   return (
     <div className='bg-black bg-opacity-95 w-[100vw] h-full'>
         
       <div className='h-full flex justify-center items-center'>
-        {/* <RatingComponent /> */}
-        <Thankyou />
+        <RatingComponent />
       </div>
     </div>
   )
