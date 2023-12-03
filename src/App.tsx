c91135ca186e926acc4947516c7f7de841e76a39
Erudite885 <erudite885@gmail.com>
2023-12-03 21:44:31 +1300

    backgroound adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import RatingComponent from "./components/RatingComponent"
 
 const App: React.FC = () => {
   return (
-    <div className='bg-black bg-opacity-95 w-[100vw] h-full'>
+    <div className='bg-black bg-opacity-30 w-[100vw] h-full'>
         
       <div className='h-full flex justify-center items-center'>
         <RatingComponent />
